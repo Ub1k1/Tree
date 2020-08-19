5409bc14145a1e2e499cccad27a62a25844fb3b0
Ub1k1 <63819671+Ub1k1@users.noreply.github.com>
2020-08-19 19:29:23 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Node {
+    Node left;
+    Node right;
+//    Node parent;
+    int data;
+
+    public Node(int value){
+        data = value;
+        left = null;
+        right = null;
+    }
+}
